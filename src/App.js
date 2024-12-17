ee1d08f34b26ca999beb23861a398e7c0518bd16
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2024-12-17 15:09:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Phylogeny} from './phylogeny'
+
+function App() {
+  return (
+    <div >
+      <Phylogeny />
+      
+    </div>
+  );
+}
+
+export default App;
